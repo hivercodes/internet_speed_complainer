8ee861d9d235f59bcb9e9d8501b879aeb432c4da
Lars <lars.eriksson.mail@gmail.com>
2022-04-04 18:31:51 +0200

    Got the webscraping for the internet speeds working.

---
+++ main.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+PROMISED_DOWN = 500
+PROMISED_UP = 50
+
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://www.speedtest.net/")
+
+go_button_1 = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+go_button_1.click()
+time.sleep(60)
+download = round(float(driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text))
+upload = round(float(driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text))
+
+print(download)
+print(upload)
+
